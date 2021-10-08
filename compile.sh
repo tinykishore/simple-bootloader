177fb0b81a4ea95dfb3ca8f0604baf54625c4f30
Kishore Shanto <tinykishore@gmail.com>
2021-10-08 12:58:58 +0600

    updated project skeleton

--- compile.sh
+++ compile.sh
@@ -1,4 +1,11 @@
-mkdir bin
+if [ ! -f out ]; then
+    mkdir out
+fi
+
+if [ ! -f bin ]; then
+    mkdir bin
+fi
+
 nasm sector1/bootloader.asm -f bin -o bin/bootloader.bin
 nasm sector2/exProgram.asm -f bin -o bin/exProgram.bin
 
